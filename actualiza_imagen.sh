68e2cfcce362960b550491211ed730d0dbb30650
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-03-20 18:18:50 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -3,6 +3,7 @@ cd /home/pi/IMAGEN_PHP
 git fetch --all
 sleep 5
 git reset --hard origin/main
+echo "Imagen actualizada a la última versión disponible en el repositorio de GitHub"
 sleep 5
 sudo chmod 777 -R /home/pi/IMAGEN_PHP
 
