e90d95cf1fd28e717954107d2d93bdb6abf34d30
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-03-20 17:42:43 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -7,6 +7,7 @@
                         cd /home/pi/IMAGEN_PHP                                             
                         git pull --force                                                                
                         sudo chmod 777 -R /home/pi/IMAGEN_PHP
+                        sleep 3
                         
 
                         
