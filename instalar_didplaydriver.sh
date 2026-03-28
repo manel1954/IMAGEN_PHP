fc6b22ae5074cc90d2a2b983cb05790893d4669d
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-03-28 18:58:10 +0100

    Create instalar_didplaydriver.sh

---
+++ instalar_didplaydriver.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+git clone https://github.com/g4klx/Display-Driver.git
+cd Display-Driver
+make
+sudo make install
+
+sudo chmod 777 -R ~/Display-Driver
+
+
+sudo apt-get install -y php-zip
+sudo systemctl restart apache2
+sudo nano /etc/php/8.2/apache2/php.ini
+
+sudo cat > /etc/systemd/system/displaydriver.service <<- "EOF"
+[Unit]
+Description=MMDVM Display-Driver for Nextion via MQTT
+After=mosquitto.service mmdvmhost.service
+
+[Service]
+Type=simple
+User=root
+WorkingDirectory=/home/pi//Display-Driver
+ExecStart=/home/pi/Display-Driver/DisplayDriver DisplayDriver.ini
+Restart=always
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable displaydriver.service
+sudo systemctl start displaydriver.service
+
+#grep -q "www-data ALL=(ALL) NOPASSWD: /bin/systemctl restart displaydriver.service" /etc/sudoers || echo "www-data ALL=(ALL) NOPASSWD: /bin/systemctl restart displaydriver.service" | sudo EDITOR='tee -a' visudo
+
