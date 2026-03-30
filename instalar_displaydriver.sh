fe3b6c763d386c73e7bd2abc98cde88b9c54e8dc
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-03-30 19:18:53 +0200

    Update instalar_displaydriver.sh

--- instalar_displaydriver.sh
+++ instalar_displaydriver.sh
@@ -5,9 +5,6 @@ VERDE="\033[1;32m"
 AMARILLO="\033[1;33m"
 GRIS="\033[0m"
 
-set -e
-
-
 if [ -d "/home/pi/A108" ]; then
         cd /home/pi/A108
         sudo sh servicio_displaydriver.sh
@@ -16,22 +13,14 @@ if [ -d "/home/pi/A108" ]; then
     fi
 
 sudo chmod 777 /etc/systemd/system/displaydriver.service
-#sudo systemctl daemon-reload
-#sleep 2
-#sudo systemctl enable displaydriver.service
-#sleep 2
-#sudo systemctl start displaydriver.service
 
-
+set -e
 if [ -d "/home/pi/Display-Driver" ]; then
     #echo -e "${VERDE}"
     echo "********************************************************"
     echo "         Display-Driver ya ha sido instalado."
     echo "********************************************************"
-    #echo -e "${AMARILLO}"
-    #echo "PULSA ENTER PARA CONTINUAR"
-    #read -r a
-    #clear
+
 else
     cd /home/pi
     git clone https://github.com/g4klx/Display-Driver.git
