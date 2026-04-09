b64d32c35dceb9ad6c55abc6364fb6ec5d5f9c2e
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-09 21:35:12 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -8,9 +8,7 @@
                         cp -R /home/pi/IMAGEN_PHP/html/ /var/www/
                         sleep 6                                             
                         sudo chmod 777 -R /home/pi/A108   
-                        sudo chmod 777 -R /var/www/html   
-                        sudo chmod 777 -R /home/pi/MMDVMHost
-                   
+                        sudo chmod 777 -R /var/www/html               
 
                         
                  
