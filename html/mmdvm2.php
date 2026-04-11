76b1b8881c671b1dd667a915f30421e66622955e
manel1954 <55663831+manel1954@users.noreply.github.com>
2026-04-11 12:48:33 +0200

    Update mmdvm2.php

--- html/mmdvm2.php
+++ html/mmdvm2.php
@@ -622,7 +622,7 @@ button.btn-header { font-family: var(--font-mono); }
 <header class="ctrl-header">
 <img src="Logo_ea3eiz.png" alt="EA3EIZ" style="height:40px;width:auto;">
 <h1>PANEL SISTEMAS DIGITALES</h1>
-<!-- <span class="uptime" id="clock">--:--:--</span> -->
+<span class="uptime" id="clock">--:--:--</span>
 <a href="edit_ini.php?file=displaydriver" target="_blank" class="btn-header cyan"> 📄 Config Display-Driver </a>
 <a href="?action=backup-configs" class="btn-header amber"> 💾 Backup Configs </a>
 <button onclick="openRestore()" class="btn-header cyan"> 📂 Restore Configs </button>
